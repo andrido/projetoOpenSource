9097460d1e0346b3167d7f495b9d5f85634b1126
BritoRuan <ruan.desenvolvedor@gmail.com>
2023-12-03 10:33:13 -0300

    [Updated] Updated validate token file.

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,24 +3,20 @@ const jwt = require('jsonwebtoken')
 const knex = require('../database/connection')
 
 const validateToken = async (req, res, next) => {
-    const { authorization } = req.headers;
-    if (!authorization) {
+    const { authorization } = req.headers
+
+    if(!authorization){
         return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
     }
+
     const token = authorization.split(' ')[1]
 
     try {
-        const { id } = jwt.verify(token, process.env.JWT_PASS)
-        const generatorToken = await knex('usuarios').select('*').where({id}).debug()
-        const generatorTokenCount = await knex('usuarios').select('*').where({id}).count().debug()
+        const { sub } = jwt.verify(token, process.env.JWT_PASS)
 
+        req.user = { id: sub }
 
-        if (generatorTokenCount === 0) {
-            return res.status(404).json({ mensagem: 'Usuário não existe' })
-        }
-        const { password: _, ...user } = generatorToken
-        req.user = user
-        return next();
+    next()
     } catch (error) {
         return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
     }
