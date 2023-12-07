f4f1062ed8b06184ed86b1d67bab6e2e6bcded18
andrido <matheusandradeskr@gmail.com>
2023-12-07 20:12:36 -0300

    [Deleted] Deleted routes Categories file

--- src/routes/routesCategories.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerCategories = express();
-const { listCategories } = require('../controllers/controllerscategories');
-
-
-routerCategories.get('/categories', listCategories);
-
-module.exports = routerCategories;
-
