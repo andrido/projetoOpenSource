5a814784c9e13cc0938bf7f417d471aa77df61e5
andrido <matheusandradeskr@gmail.com>
2023-12-07 20:10:07 -0300

    [Updated] Updated middleware user file

--- src/middlewares/middlewareUser.js
+++ src/middlewares/middlewareUser.js
@@ -1,13 +1,11 @@
-const middlewareRegisterUser = joiSchema => async (req,res,next) => {
-
+const middlewareRegisterUser = joiSchema => async (req, res, next) => {
     try {
         await joiSchema.validateAsync(req.body)
         next()
     } catch (error) {
-        return res.status(400).json({message: error.message})
-        
-    }
 
+        return res.status(400).json({ message: error.message })
+    }
 }
 
 module.exports = {
