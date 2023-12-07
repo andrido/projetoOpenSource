beb8196d6e777d23ab86cffcabb08e1b0469bbd6
andrido <matheusandradeskr@gmail.com>
2023-12-07 20:12:09 -0300

    [Updated] Updated index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,23 @@
-const route = require('express').Router();
-const routesUser = require('./routesUser');
-const routesCategories = require('./routesCategories');
+const express = require('express');
+const route = express()
 
-route.use(routesUser, routesCategories);
+const { registerUser, login, userDetails, editUser } = require('../controllers/controllersUser');
+const { middlewareRegisterUser } = require('../middlewares/middlewareUser');
+const { listCategories } = require('../controllers/controllerscategories');
+
+const schemaUser = require('../schemas/schemaUser');
+const schemaLogin = require('../schemas/schemaLogin');
+const { validateToken } = require('../middlewares/validateToken');
+
+
+route.get('/categoria', listCategories);
+route.post('/usuario', middlewareRegisterUser(schemaUser), registerUser);
+route.post('/login', middlewareRegisterUser(schemaLogin), login);
+
+route.use(validateToken)
+
+route.put('/usuario', middlewareRegisterUser(schemaUser), editUser)
+route.get('/usuario', userDetails)
 
 
 module.exports = route;
