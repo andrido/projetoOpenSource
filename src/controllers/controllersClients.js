27fca41ac8d79d22c3d885ee9892905929f25220
BritoRuan <ruan.desenvolvedor@gmail.com>
2023-12-09 16:32:58 -0300

    [Added] Added Clients Controllers file.

---
+++ src/controllers/controllersClients.js
@@ -0,0 +1,29 @@
+const knex = require('../database/connection');
+
+const clientRegister = (req, res) => {
+
+}
+
+const clientList = (req, res) => {
+
+}
+
+const clientDetail = (req, res) => {
+
+}
+
+const clientEdit= (req, res) => {
+
+}
+
+const clientDelete = (req, res) => {
+
+}
+
+module.exports = {
+ clientRegister,
+ clientList,
+ clientDetail,
+ clientEdit,
+ clientDelete
+}
