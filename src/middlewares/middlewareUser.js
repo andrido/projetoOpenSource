da8982b7271a85b633bf0cc4dbe4130e213481df
BritoRuan <ruan.desenvolvedor@gmail.com>
2023-12-09 16:11:47 -0300

    [Fix] Fix of possible problems and code aesthetics

--- src/middlewares/middlewareUser.js
+++ src/middlewares/middlewareUser.js
@@ -3,11 +3,7 @@ const middlewareRegisterUser = joiSchema => async (req, res, next) => {
         await joiSchema.validateAsync(req.body)
         next()
     } catch (error) {
-
-
-      
         return res.status(400).json({ message: error.message })
-
     }
 }
 
